2a43b1c53b4c322f1066b10c1251a93cb6227e95
Jarred Perez <jarredperez50@gmail.com>
2021-04-22 22:37:51 -0400

    added green color option

--- Downloads/Advanced Programming/letspaint.py
+++ Downloads/Advanced Programming/letspaint.py
@@ -17,6 +17,8 @@ def aRectangle(win):
         rect.setFill('yellow')
     elif color1 == "n":
         rect.setFill('brown')
+    elif color1 == "g":
+        rect.setFill('green')
     rect.draw(win)
     pass
 
@@ -37,13 +39,15 @@ def aLine(win):
         l.setOutline('yellow')
     elif color1 == "n":
         l.setOutline('brown')
+    elif color1 == "g":
+        l.setFill('green')
     l.draw(win)
     pass
 
 def aCircle(win):
     c1 = win.getMouse()
     c2 = win.getMouse()
-    c3 = Entry(Point(500, 60), 3)
+    c3 = Entry(Point(500, 85), 3)
     circ = Circle((c1, c2), c3)  
     color1 = win.getKey()
     if color1 == "w":
@@ -58,6 +62,8 @@ def aCircle(win):
         circ.setFill('yellow')
     elif color1 == "n":
         circ.setFill('brown')
+    elif color1 == "g":
+        circ.setFill('green')
     circ.draw(win)
     pass
 
@@ -79,6 +85,8 @@ def aPolygon(win):
         poly.setFill('yellow')
     elif color1 == "n":
         poly.setFill('brown')
+    elif color1 == "g":
+        poly.setFill('green')
     poly.draw(win)
     pass
 
@@ -88,11 +96,13 @@ def main():
     win.setBackground('light gray')
     condition = True
     while condition:
-        message = Text(Point(500, 30), 'Press l to draw a Line, s to draw a Rectangle, c to draw a Circle, and p to draw a Polygon')
-        message.draw(win)
-        message_c = Text(Point(500, 50), 'Press w for White, k for Black, b for Blue, r for Red, y for Yellow, and n for Brown')
+        message1 = Text(Point(500, 30), 'Press l to draw a Line, s to draw a Rectangle, c to draw a Circle, and p to draw a Polygon')
+        message1.draw(win)
+        message2 = Text(Point(500,  45), 'After selecting the figure you wish to draw, use your mouse to click on the points where you wish to draw')
+        message2.draw(win)
+        message_c = Text(Point(500, 60), 'Then, press w for White, k for Black, b for Blue, r for Red, y for Yellow, n for Brown, and g for Green')
         message_c.draw(win)
-        option = win.getKey()
+        option = win.getKey() 
         if option == "l":
             aLine(win)
         elif option == "s":
@@ -101,12 +111,12 @@ def main():
             aCircle(win)
         elif option == "p":
             aPolygon(win)
-        goodbye_m = Text(Point(500, 70), 'If you wish to continue drawing press 1, if you wish to close the program press esc')
+        goodbye_m = Text(Point(500, 75), 'After drawing each figure, if you wish to continue drawing press 1 and proceed to do the same steps, if you wish to close the program press 0')
         goodbye_m.draw(win)
         goodbye_k = win.getKey()
         if goodbye_k == "1":
              condition = True
-        elif goodbye_k == "esc":
+        elif goodbye_k == "0":
              win.close()
     
     pass
